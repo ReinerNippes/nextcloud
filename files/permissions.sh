edd1117ba85e97db3a16c22536d8c28276683cef
Reiner Nippes <reiner@nippes.it>
2018-03-03 17:05:04 +0000

    centos7

--- files/permissions.sh
+++ files/permissions.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 find /var/www/ -type f -print0 | xargs -0 chmod 0640
 find /var/www/ -type d -print0 | xargs -0 chmod 0750
-chown -R www-data:www-data /var/www/
-chown -R www-data:www-data /upload_tmp/
-chown -R www-data:www-data $NC_DATADIR
+chown -R $WEB_USER:$WEB_GROUP /var/www/
+chown -R $WEB_USER:$WEB_GROUP /upload_tmp/
+chown -R $WEB_USER:$WEB_GROUP $NC_DATADIR
 chmod 0644 /var/www/nextcloud/.htaccess
 chmod 0644 /var/www/nextcloud/.user.ini
 chmod 600 /etc/letsencrypt/live/$FQDN/fullchain.pem
