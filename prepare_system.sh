358665bc3b72fc3527bf311315dc81a22b62566e
Reiner Nippes <reiner@nippes.it>
2019-05-01 22:03:02 +0000

    arm support

--- prepare_system.sh
+++ prepare_system.sh
@@ -1,63 +1,82 @@
-#!/bin/bash
+#!/bin/bash -uxe
 #
-# Prepare system for nextcloud
+# Prepare system for nextcloud devel
 #
-prepare_ubuntu() {
-        $SUDO apt install software-properties-common -y
-        $SUDO apt-add-repository ppa:ansible/ansible -y
-        $SUDO apt update -y
-        $SUDO apt install ansible python-mysqldb python-netaddr mc vim git facter -y
-        echo
-        echo "Ubuntu Sytem ready for nextcloud."
-        echo
+
+install_pip () {
+	curl https://bootstrap.pypa.io/get-pip.py | $SUDO $PYTHON_BIN
+	$SUDO pip install setuptools -U
+	$SUDO pip install ansible -U
+	$SUDO pip install netaddr -U
+	$SUDO pip install dnspython -U
+	$SUDO pip install passlib -U
+	$SUDO pip install bcrypt -U
 }
 
-prepare_debian() {
-        $SUDO apt install dirmngr mc vim git facter -y
-        $SUDO apt update -y
-        $SUDO apt install python-mysqldb python-pip python3-pip facter -y
-        $SUDO pip install pip -U
-        $SUDO pip install setuptools -U
-        $SUDO pip install ansible -U
-        echo
-        echo "Debian Sytem ready for nextcloud."
-        echo
+prepare_ubuntu() { 
+	$SUDO apt update -y
+	$SUDO apt dist-upgrade -y
+	$SUDO apt install software-properties-common curl git mc vim facter python-minimal -y
+	$SUDO [ $(uname -m) == "aarch64" ] && apt install gcc python-dev libffi-dev libssl-dev make -y
+
+	PYTHON_BIN=/usr/bin/python
+	install_pip
+		
+	echo
+	echo "Ubuntu Sytem ready for nextcloud." 
+	echo
+}
+
+prepare_debian() { 
+	$SUDO apt update -y
+	$SUDO apt dist-upgrade -y
+	$SUDO apt install dirmngr curl git mc vim facter python -y
+	$SUDO [ $(uname -m) == "aarch64" ] && apt install gcc python-dev libffi-dev libssl-dev make -y
+	
+	PYTHON_BIN=/usr/bin/python
+	install_pip
+	
+	echo
+	echo "Debian Sytem ready for nextcloud."
+	echo
 }
 
 prepare_raspbian() {
-        $SUDO apt install dirmngr mc vim git libffi-dev facter -y
-        $SUDO apt dist-upgrade -y
-        $SUDO apt install python-mysqldb python-pip python3-pip facter -y
-        $SUDO pip install pip -U
-        $SUDO pip install setuptools -U
-        $SUDO pip install ansible -U
-        echo
-        echo "Rasbpian System ready for nextcloud."
-        echo
+	$SUDO apt update -y
+	$SUDO apt dist-upgrade -y
+	$SUDO apt install dirmngr mc vim git libffi-dev curl facter -y
+	PYTHON_BIN=/usr/bin/python
+	install_pip
+	
+	echo
+	echo "Rasbpian System ready for nextcloud."
+	echo
 }
 
-prepare_centos() {
-        $SUDO yum install epel-release -y
-        $SUDO yum install ansible git vim mc python-mysqldb python-netaddr facter -y
-        $SUDO yum update -y
-        echo
-        echo "CentOS Sytem ready for nextcloud."
-        echo
+prepare_centos() { 
+	$SUDO yum install epel-release -y
+	$SUDO yum install git vim mc curl facter libselinux-python -y
+	$SUDO yum update -y
+	PYTHON_BIN=/usr/bin/python
+	install_pip
+	
+	echo
+	echo "CentOS Sytem ready for nextcloud."
+	echo
 }
 
-usage() {
-        echo
-        echo "Linux distribution not detected."
-        echo "Use: IB=[Ubuntu|Debian|CentOS|raspbian] setup_ec2.sh"
-        echo "Other distributions not yet supported."
-        echo
+usage() { 
+	echo
+	echo "Linux distribution not detected."
+	echo "Use: ID=[Ubuntu|Debian|CentOS|raspbian] setup_ec2.sh"
+	echo "Other distributions not yet supported."
+	echo
 }
 
-# get infos about linux distro
-if [  -f /etc/os-release ]; then
-        . /etc/os-release
+if [  -f /etc/os-release ]; then 
+	. /etc/os-release
 elif [ -f /etc/debian_version ]; then
-        $ID=debian
+	$ID=debian
 fi
 
 # root or not
@@ -68,19 +87,19 @@ else
 fi
 
 case $ID in
-        'ubuntu')
-                prepare_ubuntu
-        ;;
-        'debian')
-                prepare_debian
-        ;;
-        'raspbian')
-                prepare_raspbian
-        ;;
-        'centos')
-                prepare_centos
-        ;;
-        *)
-                usage
-        ;;
+	'ubuntu')
+		prepare_ubuntu
+	;;
+	'debian')
+		prepare_debian
+	;;
+	'raspbian')
+		prepare_raspbian
+	;;
+	'centos')
+		prepare_centos
+	;;
+	*)
+		usage
+	;;
 esac
