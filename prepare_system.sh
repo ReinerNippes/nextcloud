30f947291f7ccbd2b219241a5cc3b77a098282dd
Reiner Nippes <reiner@nippes.it>
2019-08-23 15:02:49 +0000

    fedora support added

--- prepare_system.sh
+++ prepare_system.sh
@@ -4,83 +4,119 @@
 #
 
 install_pip () {
-	curl https://bootstrap.pypa.io/get-pip.py | $SUDO $PYTHON_BIN
-	$SUDO $PIP_BIN install setuptools -U
-	$SUDO $PIP_BIN install ansible -U
-	$SUDO $PIP_BIN install netaddr -U
-	$SUDO $PIP_BIN install dnspython -U
-	$SUDO $PIP_BIN install passlib -U
-	$SUDO $PIP_BIN install bcrypt -U
+        curl https://bootstrap.pypa.io/get-pip.py | $SUDO $PYTHON_BIN
+        $SUDO pip install setuptools -U
+        $SUDO pip install ansible -U
+        $SUDO pip install netaddr -U
+        $SUDO pip install dnspython -U
+        $SUDO pip install passlib -U
+        $SUDO pip install bcrypt -U
 }
 
-prepare_ubuntu() { 
-	$SUDO apt update -y
-	$SUDO apt dist-upgrade -y
-	$SUDO apt install software-properties-common curl git mc vim facter python3 python3-distutils python3-requests -y
-	$SUDO [ $(uname -m) == "aarch64" ] && apt install gcc python3-dev libffi-dev libssl-dev make -y
+prepare_ubuntu() {
+        $SUDO apt update -y
+        $SUDO apt dist-upgrade -y
+        $SUDO apt install software-properties-common curl git mc vim facter python aptitude -y
+        $SUDO [ $(uname -m) == "aarch64" ] && apt install gcc python-dev libffi-dev libssl-dev make -y
 
-	PYTHON_BIN=/usr/bin/python3
-	PIP_BIN=/usr/local/bin/pip
-	install_pip
-		
-	echo
-	echo "Ubuntu Sytem ready for nextcloud." 
-	echo
+        PYTHON_BIN=/usr/bin/python
+        install_pip
+        $SUDO pip install python-apt -U
+
+        set +x
+        echo
+        echo "   Ubuntu Sytem ready for nextcloud."
+        echo
+        ansible --version
 }
 
-prepare_debian() { 
-	$SUDO apt update -y
-	$SUDO apt dist-upgrade -y
-	$SUDO apt install dirmngr curl git mc vim facter python3 python3-distutils python3-requests aptitude -y
-	$SUDO [ $(uname -m) == "aarch64" ] && apt install gcc python3-dev libffi-dev libssl-dev make -y
-	
-	PYTHON_BIN=/usr/bin/python3
-	PIP_BIN=/usr/local/bin/pip
-	install_pip
-	
-	echo
-	echo "Debian Sytem ready for nextcloud."
-	echo
+prepare_debian() {
+        $SUDO apt update -y
+        $SUDO apt dist-upgrade -y
+        $SUDO apt install dirmngr curl git mc vim facter python aptitude -y
+        $SUDO [ $(uname -m) == "aarch64" ] && apt install gcc python-dev libffi-dev libssl-dev make -y
+
+        PYTHON_BIN=/usr/bin/python
+        install_pip
+        $SUDO pip install python-apt -U
+
+        set +x
+        echo
+        echo "   Debian Sytem ready for nextcloud."
+        echo
+        ansible --version
 }
 
 prepare_raspbian() {
-	$SUDO apt update -y
-	$SUDO apt dist-upgrade -y
-	$SUDO apt install dirmngr mc vim git libffi-dev curl facter -y
-	PYTHON_BIN=/usr/bin/python
-	install_pip
-	
-	echo
-	echo "Rasbpian System ready for nextcloud."
-	echo
+        $SUDO apt update -y
+        $SUDO apt dist-upgrade -y
+        $SUDO apt install dirmngr mc vim git libffi-dev curl facter -y
+        PYTHON_BIN=/usr/bin/python
+        install_pip
+
+        set +x
+        echo
+        echo "   Rasbpian System ready for nextcloud."
+        echo
+        ansible --version
 }
 
-prepare_centos() { 
-	$SUDO yum install epel-release -y
-	$SUDO yum install git vim mc curl facter libselinux-python python36 python36-pip -y
-	$SUDO yum update -y
+prepare_centos() {
+        $SUDO yum install epel-release -y
+        $SUDO yum install git vim mc curl facter libselinux-python python -y
+        $SUDO yum update -y
 
-	PYTHON_BIN=/usr/bin/python3.6
-	PIP_BIN=/usr/local/bin/pip
-	install_pip
-	
-	echo
-	echo "CentOS Sytem ready for nextcloud."
-	echo
+        PYTHON_BIN=/usr/bin/python
+        install_pip
+
+        set +x
+        echo
+        echo "   CentOS Sytem ready for nextcloud."
+        echo
+        ansible --version
 }
 
-usage() { 
-	echo
-	echo "Linux distribution not detected."
-	echo "Use: ID=[Ubuntu|Debian|CentOS|raspbian] setup_ec2.sh"
-	echo "Other distributions not yet supported."
-	echo
+prepare_fedora() {
+        $SUDO yum install git vim mc curl facter libselinux-python python python3-dnf -y
+        $SUDO yum update -y
+
+        PYTHON_BIN=/usr/bin/python
+        install_pip
+
+        set +x
+        echo
+        echo "   Fedora Sytem ready for nextcloud."
+        echo
+        ansible --version
 }
 
-if [  -f /etc/os-release ]; then 
-	. /etc/os-release
+prepare_amzn() {
+        $SUDO amazon-linux-extras install epel -y
+        $SUDO yum install git vim mc curl facter libselinux-python python -y
+        $SUDO yum update -y
+
+        PYTHON_BIN=/usr/bin/python
+        install_pip
+
+        set +x
+        echo
+        echo "   Amazon Linux 2 ready for nextcloud."
+        echo
+        ansible --version
+}
+
+usage() {
+        echo
+        echo "Linux distribution not detected."
+        echo "Use: ID=[ubuntu|debian|centos|raspbian|amzn] prepare_system.sh"
+        echo "Other distributions not yet supported."
+        echo
+}
+
+if [  -f /etc/os-release ]; then
+        . /etc/os-release
 elif [ -f /etc/debian_version ]; then
-	$ID=debian
+        $ID=debian
 fi
 
 # root or not
@@ -91,19 +127,27 @@ else
 fi
 
 case $ID in
-	'ubuntu')
-		prepare_ubuntu
-	;;
-	'debian')
-		prepare_debian
-	;;
-	'raspbian')
-		prepare_raspbian
-	;;
-	'centos')
-		prepare_centos
-	;;
-	*)
-		usage
-	;;
+        'ubuntu')
+                prepare_ubuntu
+        ;;
+        'debian')
+                prepare_debian
+        ;;
+        'raspbian')
+                prepare_raspbian
+        ;;
+        'centos')
+                prepare_centos
+        ;;
+        'fedora')
+                prepare_fedora
+        ;;
+        'amzn')
+                prepare_amzn
+        ;;
+
+        *)
+                usage
+        ;;
 esac
+
