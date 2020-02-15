cc412c5a9efd2051f01f6b12d555ef31e0ab7ab1
Reiner Nippes <reiner@nippes.it>
2020-02-15 19:54:27 +0000

    python 3 default interpreter

--- prepare_system.sh
+++ prepare_system.sh
@@ -5,56 +5,76 @@
 
 prepare_ubuntu() {
         $SUDO apt update -y
-        $SUDO apt-get -o Dpkg::Options::="--force-confnew" --force-yes -fuy dist-upgrade
-        $SUDO apt-get -o Dpkg::Options::="--force-confnew" --force-yes -fuy install software-properties-common curl git mc vim facter python3 python3-setuptools python3-apt python3-pip python3-passlib python3-wheel python3-bcrypt aptitude -y
-        $SUDO [ $(uname -m) == "aarch64" ] && apt install gcc python3-dev libffi-dev libssl-dev make -y
+        $SUDO apt-get -o Dpkg::Options::="--force-confold" -fuy dist-upgrade
+        $SUDO apt-get -o Dpkg::Options::="--force-confold" -fuy install software-properties-common curl git mc vim facter python3 python3-setuptools python3-apt python3-pip python3-passlib python3-wheel python3-bcrypt aptitude -y
+        $SUDO [ $(uname -m) == "aarch64" ] && $SUDO apt install gcc python3-dev libffi-dev libssl-dev make -y
         $SUDO pip3 install ansible -U
 
         set +x
         echo
-        echo "   Ubuntu Sytem ready for nextcloud."
+        echo "------------------------------------------------------"
+        echo
+        echo "   Ubuntu System ready to install nextcloud."
         echo
         ansible --version
+        echo
+        echo "------------------------------------------------------"
+        echo
 }
 
 prepare_debian() {
         $SUDO apt update -y
-        $SUDO apt-get -o Dpkg::Options::="--force-confnew" --force-yes -fuy dist-upgrade
-        $SUDO apt-get -o Dpkg::Options::="--force-confnew" --force-yes -fuy install dirmngr curl git mc vim facter python3 python3-pip python3-apt python3-passlib python3-bcrypt python3-wheel python3-setuptools aptitude ansible -y
+        $SUDO apt-get -o Dpkg::Options::="--force-confold" -fuy dist-upgrade
+        $SUDO apt-get -o Dpkg::Options::="--force-confnew" -fuy install dirmngr curl git mc vim facter python3 python3-pip python3-apt python3-passlib python3-bcrypt python3-wheel python3-setuptools aptitude ansible -y
         [ $(uname -m) == "aarch64" ] && $SUDO apt install gcc python3-dev libffi-dev libssl-dev make -y
         $SUDO pip3 install ansible -U
 
         set +x
         echo
-        echo "   Debian Sytem ready for nextcloud."
+        echo "------------------------------------------------------"
+        echo
+        echo "   Debian System ready to install nextcloud."
         echo
         ansible --version
+        echo
+        echo "------------------------------------------------------"
+        echo
 }
 
 prepare_raspbian() {
         $SUDO apt update -y
-        $SUDO DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confnew" --force-yes -fuy dist-upgrade
+        $SUDO apt-get -o Dpkg::Options::="--force-confold" -fuy dist-upgrade
         $SUDO apt install dirmngr mc vim git libffi-dev curl facter python python-apt python-pip python-passlib python-bcrypt aptitude ansible -y
         $SUDO pip install ansible -U
 
         set +x
         echo
-        echo "   Rasbpian System ready for nextcloud."
+        echo "------------------------------------------------------"
+        echo
+        echo "   Rasbpian System ready to install nextcloud."
         echo
         ansible --version
+        echo
+        echo "------------------------------------------------------"
+        echo
 }
 
 prepare_centos() {
         $SUDO yum install epel-release -y
-        $SUDO yum install git vim mc curl facter libselinux-python python python-pip python-passlib python-bcrypt aptitude ansible -y
+        $SUDO yum install git vim mc curl facter python36 python36-pip ansible -y
         $SUDO yum update -y
-        $SUDO pip install ansible -U
+        $SUDO pip3 install ansible -U
 
         set +x
         echo
-        echo "   CentOS Sytem ready for nextcloud."
+        echo "------------------------------------------------------"
+        echo
+        echo "   CentOS System ready to install nextcloud."
         echo
         ansible --version
+        echo
+        echo "------------------------------------------------------"
+        echo
 }
 
 prepare_fedora() {
@@ -63,22 +83,35 @@ prepare_fedora() {
 
         set +x
         echo
-        echo "   Fedora Sytem ready for nextcloud."
+        echo "------------------------------------------------------"
+        echo
+        echo "   Fedora System ready to install nextcloud."
         echo
         ansible --version
+        echo
+        echo "------------------------------------------------------"
+        echo
 }
 
 prepare_amzn() {
         $SUDO amazon-linux-extras install epel -y
-        $SUDO yum install git vim mc curl facter libselinux-python python python-pip python-passlib python-bcrypt aptitude ansible -y
+        $SUDO amazon-linux-extras install python3 -y
+        $SUDO yum install git vim mc curl facter -y
         $SUDO yum update -y
-        $SUDO pip install ansible -U
+        $SUDO pip3 install ansible -U
+        $SUDO pip3 install passlib -U
+        $SUDO pip3 install bcrypt -U
 
         set +x
         echo
-        echo "   Amazon Linux 2 ready for nextcloud."
+        echo "------------------------------------------------------"
+        echo
+        echo "   Amazon Linux 2 ready to install nextcloud."
         echo
         ansible --version
+        echo
+        echo "------------------------------------------------------"
+        echo
 }
 
 usage() {
